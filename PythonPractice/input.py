02b39318ec0eb4570feca43fa58a7584e22b4452
Dadpops <dadpopsdev@gmail.com>
2023-02-05 15:35:45 -0500

    Solution 1 and 2 complete

--- PythonPractice/input.py
+++ PythonPractice/input.py
@@ -1,4 +1,23 @@
+# practicepython.org
+# solution 1
+'''
 name = input("Give me your name: ")
-age = input("Give me your age: ")
+age = int(input("Give me your age: "))
+year = 2023 + (100 - age)
 
-print("Your name is " + name + " and your age is " + age)
+print("Your name is " + name + " and you will turn 100 in the year " + str(year))
+
+'''
+
+'''
+# solution 2
+number = input("Give me a number: ")
+evenOdd = int(number) % 2
+
+if evenOdd == 0:
+    print(number + " is even")
+else:
+    print(number + " is Odd")
+
+'''
+
